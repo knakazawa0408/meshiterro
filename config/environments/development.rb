57e1e67b9862c182a8af358513e9cb5de0d15cf2
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-08-15 02:16:35 +0000

    [Add]アプリケーションを作成・準備しよう

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "79091a888d8a4049b643fd34ed1e3bb5.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.active_job.queue_adapter = :inline
 end
